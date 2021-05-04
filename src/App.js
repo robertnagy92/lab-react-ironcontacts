5343097a27a81229bab880d81eb2773d6b13573d
nagyrobert92 <nagyrobert1992@yahoo.com>
2021-05-04 18:38:53 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -23,12 +23,43 @@ const [actors, updateActors] = useState(listOfContacts)
     updateActors([elem, ...actors])
   }
 
+  const sortByAlphabet = () => {
+
+    let listClone = JSON.parse(JSON.stringify(actors))
+
+    listClone.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1
+        }
+        else if(a.name < b.name) {
+            return -1
+        }
+        else {
+            return 0
+        }
+    })
+    updateActors(listClone)
+    
+  }
+
+  const sortByPopularity = () => {
+
+    const listClone = JSON.parse(JSON.stringify(actors))
+
+    let sortedPop = listClone.sort((a, b) => b.popularity - a.popularity);
+
+    updateActors(sortedPop)
+
+  }
+
   return (
     <div >
-      <button onClick={addContact} style={{margin: "2rem"}}>Add Random Contact</button>
+      <button onClick={addContact}>Add Random Contact</button>
+      <button onClick={sortByAlphabet} style={{margin: "2rem"}}>Alphabetical order</button>
+      <button onClick={sortByPopularity}>Popularity order</button>
       {
         actors.map((element,index)=>{
-          return <ul key={index} style={{listStyleType: "none", margin: "5rem 5rem 5rem 20rem"}}>
+          return <ul key={index} style={{listStyleType: "none", margin: "1rem 1rem 1rem 20rem"}}>
                     <li><img src={element.pictureUrl} style={{width: "5rem"}}/></li>
                     <li>Name: {element.name}</li>
                     <li>Popularity: {element.popularity}</li>
