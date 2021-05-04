519bd870202a6bab13ce7d977410c141b9ee0803
nagyrobert92 <nagyrobert1992@yahoo.com>
2021-05-04 16:51:18 +0200

    first iteration done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import data from "./contacts.json"
 
 function App() {
+
+  let listOfContacts = [];
+  for(let i=1; i<=5; i++){
+    listOfContacts.push(data[i])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <table>
+     <thead>
+       <tr>
+         <th>Picture</th>
+         <th>Name</th>
+         <th>Popularity</th>
+       </tr>
+     </thead>
+     <tbody>
+       {listOfContacts.map(c => {
+         return (
+           <tr>
+             <td><img src={c.pictureUrl} style={{height: "10rem"}}></img></td>
+             <td><h3>{c.name}</h3></td>
+             <td><h4>{c.popularity}</h4></td>
+           </tr>
+         )
+       })}
+     </tbody>
+   </table>
   );
 }
 
